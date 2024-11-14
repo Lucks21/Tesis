e6b3072e54a03dd54b1e0b5afcec65eace2d9d6a
AnaysMR <manobazete21@gmail.com>
2024-11-14 20:43:17 -0300

    feat: Página principal de inicio para la aplicación WERKEN incorporación de los archivos PrincipalController y PrincipalView.blade.php

--- werken/routes/web.php
+++ werken/routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\PrincipalController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//método index de PrincipalController
+Route::get('/principal', [PrincipalController::class, 'index']);
