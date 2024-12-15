fe1ee2528093465d705f9fffb51c7c904a2ba8f6
AnaysMR <manobazete21@gmail.com>
2024-12-15 00:22:40 -0300

    feat: cambio en la ruta de la pagina principal

--- werken/routes/web.php
+++ werken/routes/web.php
@@ -3,11 +3,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/principal', [PrincipalController::class, 'index']);
+Route::get('/', [PrincipalController::class, 'index']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
